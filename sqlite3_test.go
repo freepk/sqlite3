7e1db2dfa12c6a0ea01ca3e734dd1b15195f37e8
freepk <freepk@mail.ru>
2018-11-13 00:32:47 +0300

    Next benchmarks

--- sqlite3_test.go
+++ sqlite3_test.go
@@ -24,13 +24,13 @@ func TestCommon(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer insStmt.Close()
 	for i := 0; i < 10; i++ {
-		err = insStmt.Exec(i, "test value"+strconv.Itoa(i))
+		err = insStmt.Exec(i, ("test value" + strconv.Itoa(i)))
 		if err != nil {
 			t.Fatal(err)
 		}
 	}
+	insStmt.Close()
 	selStmt, err := db.Prepare("SELECT `key`, `val` FROM `keyVal`")
 	if err != nil {
 		t.Fatal(err)
@@ -38,7 +38,6 @@ func TestCommon(t *testing.T) {
 	defer selStmt.Close()
 
 	selStmt.Exec()
-
 	i := 0
 	for selStmt.Next() {
 		i++
@@ -202,6 +201,162 @@ func BenchmarkRestore(b *testing.B) {
 		err = db.Restore("backup.db")
 		if err != nil {
 			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkNext1000Int(b *testing.B) {
+	db, err := Open(":memory:")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+	tabStmt, err := db.Prepare("CREATE TABLE `keyVal` (`key` INT NOT NULL CONSTRAINT `PK_keyVal` PRIMARY KEY, `val` TEXT NOT NULL) WITHOUT ROWID")
+	if err != nil {
+		b.Fatal(err)
+	}
+	err = tabStmt.Exec()
+	if err != nil {
+		b.Fatal(err)
+	}
+	tabStmt.Close()
+	insStmt, err := db.Prepare("INSERT INTO `keyVal` (`key`, `val`) VALUES (?1, ?2)")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < 1000; i++ {
+		err = insStmt.Exec(i, ("test value " + strconv.Itoa(i)))
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+	insStmt.Close()
+	selStmt, err := db.Prepare("SELECT `key` FROM `keyVal`")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer selStmt.Close()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		selStmt.Exec()
+		for selStmt.Next() {
+		}
+	}
+}
+
+func BenchmarkNext1000IntStr(b *testing.B) {
+	db, err := Open(":memory:")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+	tabStmt, err := db.Prepare("CREATE TABLE `keyVal` (`key` INT NOT NULL CONSTRAINT `PK_keyVal` PRIMARY KEY, `val` TEXT NOT NULL) WITHOUT ROWID")
+	if err != nil {
+		b.Fatal(err)
+	}
+	err = tabStmt.Exec()
+	if err != nil {
+		b.Fatal(err)
+	}
+	tabStmt.Close()
+	insStmt, err := db.Prepare("INSERT INTO `keyVal` (`key`, `val`) VALUES (?1, ?2)")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < 1000; i++ {
+		err = insStmt.Exec(i, ("test value " + strconv.Itoa(i)))
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+	insStmt.Close()
+	selStmt, err := db.Prepare("SELECT `key`, `val` FROM `keyVal`")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer selStmt.Close()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		selStmt.Exec()
+		for selStmt.Next() {
+		}
+	}
+}
+
+func BenchmarkNext10000Int(b *testing.B) {
+	db, err := Open(":memory:")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+	tabStmt, err := db.Prepare("CREATE TABLE `keyVal` (`key` INT NOT NULL CONSTRAINT `PK_keyVal` PRIMARY KEY, `val` TEXT NOT NULL) WITHOUT ROWID")
+	if err != nil {
+		b.Fatal(err)
+	}
+	err = tabStmt.Exec()
+	if err != nil {
+		b.Fatal(err)
+	}
+	tabStmt.Close()
+	insStmt, err := db.Prepare("INSERT INTO `keyVal` (`key`, `val`) VALUES (?1, ?2)")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < 10000; i++ {
+		err = insStmt.Exec(i, ("test value " + strconv.Itoa(i)))
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+	insStmt.Close()
+	selStmt, err := db.Prepare("SELECT `key` FROM `keyVal`")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer selStmt.Close()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		selStmt.Exec()
+		for selStmt.Next() {
+		}
+	}
+}
+
+func BenchmarkNext10000IntStr(b *testing.B) {
+	db, err := Open(":memory:")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+	tabStmt, err := db.Prepare("CREATE TABLE `keyVal` (`key` INT NOT NULL CONSTRAINT `PK_keyVal` PRIMARY KEY, `val` TEXT NOT NULL) WITHOUT ROWID")
+	if err != nil {
+		b.Fatal(err)
+	}
+	err = tabStmt.Exec()
+	if err != nil {
+		b.Fatal(err)
+	}
+	tabStmt.Close()
+	insStmt, err := db.Prepare("INSERT INTO `keyVal` (`key`, `val`) VALUES (?1, ?2)")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < 10000; i++ {
+		err = insStmt.Exec(i, ("test value " + strconv.Itoa(i)))
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+	insStmt.Close()
+	selStmt, err := db.Prepare("SELECT `key`, `val` FROM `keyVal`")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer selStmt.Close()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		selStmt.Exec()
+		for selStmt.Next() {
 		}
 	}
 }
